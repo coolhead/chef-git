797584e07dbdb96b98b9774df542b521cbd9fb48
Raghavendra S <s_raghavendra@rocketmail.com>
2015-12-04 11:42:31 +0530

    add hello.rb

---
+++ hello.rb
@@ -0,0 +1,31 @@
+puts "Hello World\n"
+bacon_type = 'crispy'
+2.times do
+  puts bacon_type
+  temperature = 300
+end
+x = "hello"
+puts "#{x} world"
+puts '#{x} world'
+<<METHOD_DESCRIPTION
+I Raghavendra learning Ruby for Chef.
+Currently I have the liability of home loan and personal loans, which I need to clear ASAP.
+
+METHOD_DESCRIPTION
+
+types = ['apple', 'mango', 'grapes']
+puts types.length
+puts types.size
+
+types.push 'orange'
+puts types
+puts types[0]
+puts types[-1]
+puts types.first
+puts types.last
+
+#Hashes
+grades = { raghu: 99, rekha: 99.99, ravi: 99.9999 }
+puts grades[:raghu]
+puts grades[:rekha]
+puts grades[:ravi]
